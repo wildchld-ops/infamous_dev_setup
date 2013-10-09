aecd4e8d36ed80ab57bd3f0071a67e7c898393ba
mrimp <mrimp@day-off.net>
2013-10-09 10:48:35 -0400

    installing on 13.10 comment

--- infamous_aokp_dev_setup.sh
+++ infamous_aokp_dev_setup.sh
@@ -44,6 +44,7 @@ chmod a+x ~/bin/repo
 echo "Done"
 echo "Installing packages"
 sleep 2
+# 13.10 remove "ia32-libs" in the string below and then run script
 sudo apt-get -y install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5-dev ia32-libs x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python
 echo "Done"
 echo "Linking..."
