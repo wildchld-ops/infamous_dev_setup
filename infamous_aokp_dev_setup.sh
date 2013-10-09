46314272822e54eca97b32daa6124bb4cc50b073
mrimp <mrimp@day-off.net>
2013-10-09 10:36:04 -0400

    Added Legal Stuff

--- infamous_aokp_dev_setup.sh
+++ infamous_aokp_dev_setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-# Setting up Android Build Enviroment Ubuntu 13.04
+#
+# Author: mrimp
+#
+# Description:
+# Setup script for AOKP Build Enviroment Ubuntu 13.04
+#
+# Legal Stuff:
+#
+# This script is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
+#
 echo "Replace openjdk with Oracle java6"
 sleep 2
 echo "Enter in your password"
