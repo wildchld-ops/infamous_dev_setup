fc612581ac9911fa4c2a0840455d46a78abef445
mrimp <mrimp@day-off.net>
2013-11-13 21:10:33 -0500

    13.10 Ready

--- infamous_aokp_dev_setup.sh
+++ infamous_aokp_dev_setup.sh
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
 #
+echo "Ubuntu 13.10"
 echo "Replace openjdk with Oracle java6"
 sleep 2
 echo "Enter in your password"
@@ -33,19 +34,20 @@ echo "Done"
 echo "Adding Webupd8 ppa for android tools"
 sleep 2
 sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-sudo apt-get update && sudo apt-get install android-tools-adb android-tools-fastboot
+sudo apt-get update
+sudo apt-get install android-tools-adb android-tools-fastboot
 echo "Done"
 echo "Create bin directory and getting repo"
 sleep 2
 rm -rf ~/bin/repo
 rm -rf ~/.repoconfig
-mkdir ~/bin && curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+mkdir ~/bin 
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 echo "Done"
 echo "Installing packages"
 sleep 2
-# 13.10 remove "ia32-libs" in the string below and then run script
-sudo apt-get -y install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5-dev ia32-libs x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python
+sudo apt-get -y install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5-dev lib32z1 lib32ncurses5 lib32bz2-1.0 x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python
 echo "Done"
 echo "Linking..."
 sleep 2
