e2f11db04d46f898a332d111352b57b28865e83e
mrimp <mrimp@day-off.net>
2014-04-20 20:09:34 -0400

    updated to 14.04

--- infamous_dev_setup.sh
+++ infamous_dev_setup.sh
@@ -26,7 +26,6 @@ echo "Enter in your password"
 sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\* -y
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update 
-sudo apt-get update
 sudo apt-get install oracle-java6-installer -y
 sudo update-java-alternatives -s java-6-oracle
 sudo apt-get install oracle-java6-set-default
@@ -49,7 +48,7 @@ chmod a+x ~/bin/repo
 echo "Done"
 echo "Installing packages"
 sleep 2
-sudo apt-get -y install -f git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5-dev lib32z1 lib32ncurses5 lib32bz2-1.0 x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush lzop schedtool libwxgtk2.8-dev python
+sudo apt-get install git gnupg ccache lzop flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32bz2-1.0 lib32ncurses5-dev x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z1-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc libreadline6-dev lib32readline-gplv2-dev libncurses5-dev bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev squashfs-tools pngcrush schedtool dpkg-dev
 echo "Done"
 echo "Linking..."
 sleep 2
